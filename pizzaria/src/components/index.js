f283ba823802432d6d9295e474bccc48d2de7118
Phelipe's <57334528+allzackk@users.noreply.github.com>
2022-03-17 20:50:59 -0300

    Delete pizzaria directory

--- pizzaria/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export { default as Card } from './source/Card.vue'
-export { default as Container } from './source/Container.vue'
-export { default as Footer } from './source/Footer.vue'
-export { default as Header } from './source/Header.vue'
-export { default as PopUp } from './source/PopUp.vue'
-export { default as Loading } from './source/Loading.vue'
-
